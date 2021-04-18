bb6996524983537563a69af45838b36f6bf3906d
vitor9 <vtorzy@gmail.com>
2021-04-18 14:05:46 -0300

    Removido o submodulo para poder add o projeto

---
+++ nest-api/src/routes/routes.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  OnModuleInit,
+} from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+
+@Controller('routes')
+export class RoutesController implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
+
+  @Post()
+  create(@Body() createRouteDto: CreateRouteDto) {
+    return this.routesService.create(createRouteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.routesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.routesService.findOne(+id);
+  }
+
+  @Patch(':id') // PATCH - Parcial e o PUT - Obriga a passagem de TODOS os campos
+  update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
+    return this.routesService.update(+id, updateRouteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.routesService.remove(+id);
+  }
+  // Ao inicializar um modulo, vai executar esse metodo - async eh assincrono
+  // await pq o processo eh assincrono. Quando nos conectarmos vamos ter a instancia do produtor
+  // Se eu tenho a instancia do meu produtor, ja posso publicar o que eu preciso.
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaClient.connect();
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    // console.log(id);
+    this.kafkaProducer.send({
+      // topicos declarados no simulador
+      topic: 'route.new-direction',
+      messages: [
+        {
+          key: 'route.new-direction',
+          value: JSON.stringify({ routeId: id, clientId: '' }),
+        },
+      ],
+    });
+  }
+}
