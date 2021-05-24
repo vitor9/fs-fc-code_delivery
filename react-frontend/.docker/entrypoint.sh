4b3b2ab77f5142305137d75b1a1fe207ca2876b0
vitor9 <vtorzy@gmail.com>
2021-05-24 20:15:54 -0300

    Teste em add projeto front ao GH

---
+++ react-frontend/.docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Eh gerado o env com a shell script
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+
+npm install
+
+# A funcao abaixo faz com que recompile o codigo sempre que uma alteracao for feita
+npm start
